d9da4abc082408f722064b6fae4c414b085bcac6
WiamBouhaj <92393892+WiamBouhaj@users.noreply.github.com>
2024-12-28 22:28:17 +0100

    Update Project.java

--- TP04-UML-Diagrams-Generator/src/org/mql/java/models/Project.java
+++ TP04-UML-Diagrams-Generator/src/org/mql/java/models/Project.java
@@ -1,11 +1,6 @@
 package org.mql.java.models;
 
 public class Project {
- private String name;
-    private List<ClassModel> classes;
-    private List<InterfaceModel> interfaces;
-
-    // Getters, setters, et constructeurs
 	public Project() {
 	}
 
